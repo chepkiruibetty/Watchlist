6ea10069752795e5a3fe838946652be9998bcf32
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-01-28 14:20:22 +0300

    add functionality to all files

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import DevConfig
+
+
 
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
@@ -8,4 +11,10 @@ app = Flask(__name__,instance_relative_config = True)
 app.config.from_object(DevConfig)
 app.config.from_pyfile("config.py")
 
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+
+
 from app import views
+from app import error
